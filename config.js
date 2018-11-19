cbc297d2de171308dca11d900381ab0ab610ea3c
Liviz <44624409+Liviz@users.noreply.github.com>
2018-11-19 21:01:35 +0300

    imit commit

---
+++ config.js
@@ -0,0 +1,1 @@
+var youtube={ID:'AO-2XBG7iFk',start:1,end:0,repeat:true,mute:false};$('#background-effect').tubular({videoId:youtube.ID+'?controls=0&end='+ youtube.end+'&rel=0&version=3',repeat:youtube.repeat,start:youtube.start,mute:youtube.mute});   
